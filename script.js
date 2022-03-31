e5636fba33a825785f72d44c93dc925aad18a0ed
Spanchyshyn <90944162+Spanchyshyn@users.noreply.github.com>
2022-03-31 11:18:11 -0700

    .

--- script.js
+++ script.js
@@ -156,3 +156,28 @@ randomWord();
 generateButtons();
 
 guessedWord();
+
+let test = document.getElementById("list");
+
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+test.addEventListener("mouseenter", function(event) {
+        // highlight the mouseenter target
+        event.target.style.color = "purple";
+
+        // reset the color after a short delay
+        setTimeout(function() {
+            event.target.style.color = "";
+        }, 500);
+    }, false);
+// This handler will be executed every time the cursor
+// is moved over a different list item
+test.addEventListener("mouseover", function(event) {
+
+        event.target.style.color = "orange";
+
+
+        setTimeout(function() {
+            event.target.style.color = "";
+        }, 500);
+    }, false);
